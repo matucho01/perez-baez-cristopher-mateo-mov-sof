f860fea2820ad83c101c67ba639574eb7f0ab63c
Mateo Pérez <cristopher.perez01@epn.edu.ec>
2023-05-25 10:51:37 -0500

    Clase Kotlin
    Primitivas, clases, clases abstractas, funciones

--- 00-kotlin/Moviles/src/main/kotlin/Main.kt
+++ 00-kotlin/Moviles/src/main/kotlin/Main.kt
@@ -1,7 +1,164 @@
+import java.util.*
+
 fun main(args: Array<String>) {
     println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // INMUTABLES (NO se reasignan "=")
+    val inmutable: String = "Adrian";
+    // inmutable = "Vicente";
+
+    // Mutables (Re asignar)
+    var mutable: String = "Vicente";
+    mutable = "Adrian";
+
+    // val > var
+
+    // Duck Typing
+    var ejemploVariable = " Mateo Perez "
+    val edadEjemplo: Int = 12
+
+    // Trim elimina espacios vacios
+    ejemploVariable.trim()
+    // ejemploVariable = edadEjemplo;
+
+    // Variable primitiva
+    val nombreProfesor: String = "Adrian Eguez"
+    val sueldo: Double = 1.2
+    val estadoCivil: Char = 'C'
+    val mayorEdad: Boolean = true
+    // Clases Java
+    val fechaNacimiento: Date = Date()
+
+    // SWITCH
+    val estadoCivilWhen = "C"
+    when (estadoCivilWhen){
+        ("C") -> {
+            println("Casado")
+        }
+        "S" -> {
+            println("Soltero")
+        }
+        else -> {
+            println("No sabemos")
+        }
+    }
+    val esSoltero = (estadoCivilWhen == "S")
+    val coqueteo = if (esSoltero) "Si" else "No"
+    calcularSueldo(10.00)
+    calcularSueldo(10.00, 12.00, 20.00)
+    calcularSueldo(10.00, bonoEspecial = 20.00) // Named Parameters
+    calcularSueldo(bonoEspecial = 20.00, sueldo = 10.00, tasa = 14.00) // Parámetros nombrados
+    val sumaUno = Suma(1,1)
+    val sumaDos = Suma(null, 1)
+    val sumaTres = Suma(1, null)
+    val sumaCuatro = Suma(null, null)
+    sumaUno.sumar()
+    sumaDos.sumar()
+    sumaTres.sumar()
+    sumaCuatro.sumar()
+    println(Suma.pi)
+    println(Suma.elevarAlCuadrado(2))
+    println(Suma.historialSumas)
+}
+
+abstract class NumerosJava{
+    protected val numeroUno: Int
+    private val numeroDos: Int
+
+    constructor(
+        uno: Int,
+        dos: Int
+    ){
+        this.numeroUno = uno
+        this.numeroDos = dos
+        println("Inicializando")
+    }
+}
+
+abstract class Numeros(
+    // Ejemplo:
+    // uno: Int, (Parametro (sin modificador de acceso))
+    // private var uno: Int, // Propiedad Publica Clase numeros.uno
+    // var uno: Int, // Propiedad de la clase (por defecto es PUBLIC)
+    // public var uno: Int,
+    protected val numeroUno: Int, // Propiedad de la clase protected numeros.numeroUno
+    protected val numeroDos: Int, // Propiedad de la clase protected numeros.numeroDos
+){
+    // var cedula: String = "" (public es por defecto)
+    // private valorCalculado: Int = 0 (private)
+    init {
+        this.numeroUno; this.numeroDos; // this es opcional
+        numeroUno; numeroDos; // sin el "this", es lo mismo
+        println("Inicializando")
+    }
+}
+
+class Suma( // Constructor Primario Suma
+    unoParametro: Int, // Parametro
+    dosParametro: Int,
+): Numeros(unoParametro, dosParametro){ // Extendiendo y mandando los parametros (super)
+    init { // Bloque codigo constructor primario
+        this.numeroUno
+        this.numeroDos
+    }
+    constructor( // Segundo constructor
+        uno: Int?, // Parametros
+        dos: Int // Parametros
+        // :this es para llamar al constructor de mi papá
+    ):this(
+        if(uno == null) 0 else uno,
+        dos
+    )
+    constructor( // Tercer constructor
+        uno: Int, // Parametros
+        dos: Int? // Parametros
+    ):this(
+        uno,
+        if(dos == null) 0 else dos
+    )
+    constructor( // Cuarto constructor
+        uno: Int?,
+        dos: Int?
+    ):this(
+        if(uno == null) 0 else uno,
+        if(dos == null) 0 else dos
+    )
+    public fun sumar(): Int {
+        val total = numeroUno + numeroDos
+        agregarHistorial(total) // this.agregarHistorial(total)
+        return total
+    }
+    // Es el equivalente a Static
+    companion object { // Atributos y metodos "Compartidos" Singletons o Static de esta clase
+        // Todas las instancias de esta clase comparten estos atributos y metodos
+        // dentro del companion Object
+        val pi = 3.14
+        fun elevarAlCuadrado(num: Int): Int{
+            return num * num
+        }
+        val historialSumas = ArrayList<Int>()
+        fun agregarHistorial(valorNuevaSuma: Int){
+            historialSumas.add(valorNuevaSuma)
+        }
+    }
+}
+
+// void -> Unit
+fun imprimirNombre(nombre: String): Unit {
+    println("Nombre : $nombre") // template strings
+}
+
+fun calcularSueldo(
+    sueldo: Double, // Requerido
+    tasa: Double = 12.00, // Opcional (defecto)
+    bonoEspecial: Double? = null,
+): Double{
+    // Int -> Int? (nullable)
+    // String -> String? (nullable)
+    // Date -> Date? (nullable)
+    if(bonoEspecial == null){
+        return sueldo * (100/tasa)
+    }else{
+        return sueldo * (100/tasa) + bonoEspecial
+    }
 }
