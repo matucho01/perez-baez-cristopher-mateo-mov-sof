545a988bf1c90976fe1ef7e57d4ba54dbf263d34
Mateo Pérez <cristopher.perez01@epn.edu.ec>
2023-06-08 10:31:29 -0500

    Cambio de AppCompat a Activity entre vistas

--- 01-Android/app/src/main/java/com/example/movilessoftware2023a/ACicloVida.kt
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/ACicloVida.kt
@@ -1,10 +1,11 @@
 package com.example.movilessoftware2023a
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ACicloVida : AppCompatActivity() {
+class ACicloVida : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aciclo_vida)
--- 01-Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
+++ 01-Android/app/src/main/java/com/example/movilessoftware2023a/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.movilessoftware2023a
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
