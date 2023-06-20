c08fcbe14baf7e8e4c335d0afa17a103e02d745d
Mateo Pérez <cristopher.perez01@epn.edu.ec>
2023-06-20 08:31:13 -0500

    Clase 13
    Creación de una nueva actividad "BListView" con dos elementos

---
+++ movilessw2023a2/app/src/main/java/com/example/movilessw2023a/BEntrenador.kt
@@ -0,0 +1,9 @@
+package com.example.movilessw2023a
+
+class BEntrenador(
+    var id: Int,
+    var nombre: String?,
+    var descripcion: String?,
+){
+
+}
--- movilessw2023a2/app/src/main/java/com/example/movilessw2023a/MainActivity.kt
+++ movilessw2023a2/app/src/main/java/com/example/movilessw2023a/MainActivity.kt
@@ -16,6 +16,13 @@ class MainActivity : AppCompatActivity() {
         botonCicloVida.setOnClickListener {
             irActividad(AACicloVida::class.java)
         }
+
+        val botonListView = findViewById<Button>(
+            R.id.btn_ir_list_view
+        )
+        botonListView.setOnClickListener {
+            irActividad(BListView::class.java)
+        }
     }
 
     fun irActividad(
