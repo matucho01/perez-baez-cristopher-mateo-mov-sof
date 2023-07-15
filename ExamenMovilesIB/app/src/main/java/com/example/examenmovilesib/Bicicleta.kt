de201faab05c698bfb5bcb5d8d7f998834da4405
Mateo Pérez <cristopher.perez01@epn.edu.ec>
2023-07-14 20:09:08 -0500

    Tablas Marca - Bicicleta

--- ExamenMovilesIB/app/src/main/java/com/example/examenmovilesib/Bicicleta.kt
+++ ExamenMovilesIB/app/src/main/java/com/example/examenmovilesib/Bicicleta.kt
@@ -1,4 +1,81 @@
 package com.example.examenmovilesib
 
 class Bicicleta {
+    var id: Int = 0
+    var modelo: String = ""
+    var tipo: String = ""
+    var anio: Int = 0
+    var precio: Double = 0.00
+    var disponible: Boolean = false
+    var marcaId: Int = 0
+
+    constructor(id: Int, modelo: String, tipo: String, anio: Int, precio: Double, marcaId: Int) {
+        this.id = id
+        this.modelo = modelo
+        this.tipo = tipo
+        this.anio = anio
+        this.precio = precio
+        this.marcaId = marcaId
+    }
+
+    fun getId(): Int {
+        return this.id
+    }
+
+    fun getModelo(): String {
+        return this.modelo
+    }
+
+    fun getTipo(): String {
+        return this.tipo
+    }
+
+    fun getAnio(): Int {
+        return this.anio
+    }
+
+    fun getPrecio(): Double {
+        return this.precio
+    }
+
+    fun getDisponible(): Boolean {
+        return this.disponible
+    }
+
+    fun getMarcaId(): Int {
+        return this.marcaId
+    }
+
+    fun setId(id: Int) {
+        this.id = id
+    }
+
+    fun setModelo(modelo: String) {
+        this.modelo = modelo
+    }
+
+    fun setTipo(tipo: String) {
+        this.tipo = tipo
+    }
+
+    fun setAnio(anio: Int) {
+        this.anio = anio
+    }
+
+    fun setPrecio(precio: Double) {
+        this.precio = precio
+    }
+
+    fun setDisponible(disponible: Boolean) {
+        this.disponible = disponible
+    }
+
+    fun setMarcaId(marcaId: Int) {
+        this.marcaId = marcaId
+    }
+
+    override fun toString(): String {
+        return "Id:$id\nModelo:$modelo\nTipo:$tipo\nAño:$anio\n" +
+                "Disponible:${if(disponible)"sí" else "no"}\nPrecio:$precio"
+    }
 }
--- ExamenMovilesIB/app/src/main/java/com/example/examenmovilesib/Marca.kt
+++ ExamenMovilesIB/app/src/main/java/com/example/examenmovilesib/Marca.kt
@@ -1,4 +1,67 @@
 package com.example.examenmovilesib
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
 class Marca {
+    private var id: Int = 0
+    private var nombre: String = ""
+    var pais: String = ""
+    private var fechaCreacion: LocalDate? = null
+    private var sede: String = ""
+
+    //var bicicletas: MutableList<Bicicleta> = mutableListOf()
+
+    constructor(id: Int, nombre: String, pais: String, fechaCreacion: LocalDate, sede: String) {
+        this.id = id
+        this.nombre = nombre
+        this.pais = pais
+        this.fechaCreacion = fechaCreacion
+        this.sede = sede
+    }
+
+    fun getId(): Int {
+        return this.id
+    }
+
+    fun getNombre(): String {
+        return this.nombre
+    }
+
+    fun getPais(): String {
+        return this.pais
+    }
+
+    fun getFechaCreacion(): LocalDate? {
+        return this.fechaCreacion
+    }
+
+    fun getSede(): String {
+        return this.sede
+    }
+
+    fun setId(id: Int) {
+        this.id = id
+    }
+
+    fun setNombre(nombre: String) {
+        this.nombre = nombre
+    }
+
+    fun setPais(pais: String) {
+        this.pais = pais
+    }
+
+    fun setFechaCreacion(fechaCreacion: LocalDate) {
+        this.fechaCreacion = fechaCreacion
+    }
+
+    fun setSede(sede: String) {
+        this.sede = sede
+    }
+
+    override fun toString(): String {
+        return "Id:$id\nNombre:$nombre\nPaís:$pais\nFecha creación:${
+            fechaCreacion!!.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))}\nSede:$sede"
+    }
 }
